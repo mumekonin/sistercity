26b6e179838208ef1294c70262a869d798b2e959
MULUALEM MEKONIN <mulualemmekonin0@gmail.com>
2026-06-22 01:58:23 -0700

    implement create user

--- src/userModule/controller/users.controller.ts
+++ src/userModule/controller/users.controller.ts
@@ -1,12 +1,12 @@
 import { Controller,Post ,Body} from "@nestjs/common";
 import {UserService }  from "../service/users.service";
 import {CreateUserDto} from"../dto/users.dto";
-@Controller()
+@Controller("/users")
 export class UsersConteroller {
   constructor(
     private readonly userService: UserService,
   ) { }
-  @Post("/users")
+  @Post("/register")
    async createUser(@Body() createUserDto:CreateUserDto ){
    const reuslt = await this.userService.createUser(createUserDto);
    return reuslt;
