590fee38238602cb7cd63e01ba1fb54e13568a16
MULUALEM MEKONIN <mulualemmekonin0@gmail.com>
2026-06-27 11:24:20 -0700

    dto and reposnes are created

---
+++ src/Documents/dto/documents.dto.ts
@@ -0,0 +1,68 @@
+import { IsString, IsNotEmpty, IsEnum, IsOptional, IsMongoId, IsDate, IsNumber, IsIn, Min, } from 'class-validator';
+import { Type } from 'class-transformer';
+import { DocumentCategory, AccessLevel, DocumentApprovalStatus, } from '../../common/enum/enum';
+export class CreateDocumentDto {
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
+  @IsEnum(DocumentCategory)
+  @IsNotEmpty()
+  category!: DocumentCategory;
+  @IsOptional()
+  @IsMongoId()
+  relatedProject?: string;
+  @IsDate()
+  @Type(() => Date)
+  documentDate!: Date;
+  @IsString()
+  @IsNotEmpty()
+  description!: string;
+  @IsEnum(AccessLevel)
+  @IsNotEmpty()
+  accessLevel!: AccessLevel;
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  expiryDate?: Date;
+  @IsString()
+  @IsNotEmpty()
+  fileUrl!: string;
+  @IsString()
+  @IsNotEmpty()
+  fileName!: string;
+  @IsString()
+  @IsNotEmpty()
+  fileType!: string;
+  @IsNumber()
+  @Min(1)
+  fileSize!: number;
+}
+export class UpdateDocumentDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(['approve', 'reject', 'archive', 'change-access'])
+  action!: 'approve' | 'reject' | 'archive' | 'change-access';
+  @IsOptional()
+  @IsString()
+  approvalNote?: string;
+  @IsOptional()
+  @IsEnum(AccessLevel)
+  accessLevel?: AccessLevel;
+}
+export class UploadNewVersionDto {
+  @IsString()
+  @IsNotEmpty()
+  fileUrl!: string;
+  @IsString()
+  @IsNotEmpty()
+  fileName!: string;
+  @IsString()
+  @IsNotEmpty()
+  fileType!: string;
+  @IsNumber()
+  @Min(1)
+  fileSize!: number;
+  @IsOptional()
+  @IsString()
+  changeNote?: string;
+}
