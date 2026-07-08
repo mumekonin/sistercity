f83b422f4cea3aa1533294af96609b898e983fd6
MULUALEM MEKONIN <mulualemmekonin0@gmail.com>
2026-07-07 23:24:17 -0700

    update the budget module

---
+++ src/Budget/budget.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+
+@Module({
+  imports: [
+        MongooseModule.forFeature([
+      {  },
+   
+    ])
+  ],
+  controllers: [],
+  providers: [],
+})
+export class BudgetModule {}
