c2db8ae1f327585a62ee72a07a905064b445b0f1
MULUALEM MEKONIN <mulualemmekonin0@gmail.com>
2026-07-11 04:23:04 -0700

    Restructure project into monorepo with backend and frontend

--- src/common/guards/jwtauth.gourds.ts
+++
@@ -1,6 +0,0 @@
-import { applyDecorators, UseGuards } from '@nestjs/common';
-import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
-
-export function JwtAuthGuard(type: string | string[] = 'jwt') {
-  return applyDecorators(UseGuards(PassportAuthGuard(type)));
-}
